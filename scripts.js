a92606c57dba62a6b1f93840f0c857e53ca073b0
TheOreMan <oreman@inbox.lv>
2023-12-20 14:38:34 +0200

    Auto commit

--- scripts.js
+++ scripts.js
@@ -1,16 +1,18 @@
 function sumIt() {
   var x=Number(document.getElementById("x").value);
   var y=Number(document.getElementById("y").value);
-
-  const response = await fetch('/calculate', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({x:x,y:y}),
-            });
-
-            const data = await response.json();
-            document.getElementById("sum").innerText=data.sum;
-
+  fetch('/calculate', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({x:x,y:y}),
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('sum').innerHTML = data.sum;
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
 }
